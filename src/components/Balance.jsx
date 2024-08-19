b3692156679dbeddac7aa1d26196a881ed462f2a
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-19 13:12:49 +0500

    Balance - Updated for useContext

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,35 +1,24 @@
-import { useState } from "react";
+import { useContext } from "react";
 import TransactionMenu from "./TransactionMenu";
+import { TransactionContext } from "../store/TransactionContext";
 
 const Balance = () => {
-  const [balance, setBalance] = useState(0);
-  const [income, setIncome] = useState(0);
-  const [expense, setExpense] = useState(0);
-
-  const onIncome = (amount) => {
-    setIncome((prevIncome) => prevIncome + amount);
-    setBalance((prevBalance) => prevBalance + amount);
-  };
-
-  const onExpense = (amount) => {
-    setExpense((prevExpense) => prevExpense + amount);
-    setBalance((prevBalance) => prevBalance - amount);
-  };
+  const { state } = useContext(TransactionContext);
 
   return (
     <div className="flex flex-col items-center gap-2">
       <p className="text-2xl mt-16">
-        Balance: <span className="font-bold">{balance}</span>
+        Balance: <span className="font-bold">{state.balance}</span>
       </p>
       <div className="flex flex-row space-x-8 mt-5 text-white">
         <p className="bg-green-800 p-2 rounded-xl">
-          Income: <span className="text-white font-bold">{income}</span>
+          Income: <span className="text-white font-bold">{state.income}</span>
         </p>
         <p className="bg-red-800 p-2 rounded-xl">
-          Expense: <span className="text-white font-bold">{expense}</span>
+          Expense: <span className="text-white font-bold">{state.expense}</span>
         </p>
       </div>
-      <TransactionMenu onIncome={onIncome} onExpense={onExpense} />
+      <TransactionMenu />
     </div>
   );
 };
