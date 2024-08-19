167b418b25c6ab9aa17db31a41f6937fedd7df08
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-19 13:44:46 +0500

    Updated Code

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,24 +1,35 @@
-import { useContext } from "react";
+import { useState } from "react";
 import TransactionMenu from "./TransactionMenu";
-import { TransactionContext } from "../store/TransactionContext";
 
 const Balance = () => {
-  const { state } = useContext(TransactionContext);
+  const [balance, setBalance] = useState(0);
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+
+  const onIncome = (amount) => {
+    setIncome(income + amount);
+    setBalance(balance + amount);
+  };
+
+  const onExpense = (amount) => {
+    setExpense(expense + amount);
+    setBalance(balance - amount);
+  };
 
   return (
     <div className="flex flex-col items-center gap-2">
       <p className="text-2xl mt-16">
-        Balance: <span className="font-bold">{state.balance}</span>
+        Balance: <span className="font-bold">{balance}</span>
       </p>
       <div className="flex flex-row space-x-8 mt-5 text-white">
         <p className="bg-green-800 p-2 rounded-xl">
-          Income: <span className="text-white font-bold">{state.income}</span>
+          Income: <span className="text-white font-bold">{income}</span>
         </p>
         <p className="bg-red-800 p-2 rounded-xl">
-          Expense: <span className="text-white font-bold">{state.expense}</span>
+          Expense: <span className="text-white font-bold">{expense}</span>
         </p>
       </div>
-      <TransactionMenu />
+      <TransactionMenu onIncome={onIncome} onExpense={onExpense} />
     </div>
   );
 };
--- src/store/TransactionContext.jsx
+++ src/store/TransactionContext.jsx
@@ -1,40 +1,33 @@
-import React, { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-// Creating a Context
-const TransactionContext = createContext();
-
-// Initial state
 const initialState = {
-  balance: 0,
   income: 0,
   expense: 0,
   transactions: [],
 };
 
-// Reducer function
 const transactionReducer = (state, action) => {
   switch (action.type) {
     case "ADD_INCOME":
       return {
         ...state,
         income: state.income + action.payload.amount,
-        balance: state.balance + action.payload.amount,
-        transactions: [action.payload, ...state.transactions],
+        transactions: [...state.transactions, action.payload],
       };
     case "ADD_EXPENSE":
       return {
         ...state,
         expense: state.expense + action.payload.amount,
-        balance: state.balance - action.payload.amount,
-        transactions: [action.payload, ...state.transactions],
+        transactions: [...state.transactions, action.payload],
       };
     default:
       return state;
   }
 };
 
-// Transaction Provider
-const TransactionProvider = ({ children }) => {
+export const TransactionContext = createContext(initialState);
+
+export const TransactionProvider = ({ children }) => {
   const [state, dispatch] = useReducer(transactionReducer, initialState);
 
   return (
